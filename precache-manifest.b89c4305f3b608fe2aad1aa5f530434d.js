93feefd7d77e8350585e471e9e4c0ec4c344953a
Stephen Wilson <stephen.ben.wilson@gmail.com>
2020-01-17 15:12:34 -0500

    Updates

---
+++ precache-manifest.b89c4305f3b608fe2aad1aa5f530434d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "377b550323779521e34be06f8418f024",
+    "url": "/Watzit/index.html"
+  },
+  {
+    "revision": "d2560725be726bb6e992",
+    "url": "/Watzit/static/css/main.322d5dc5.chunk.css"
+  },
+  {
+    "revision": "d712a12486b076316311",
+    "url": "/Watzit/static/js/2.8ad82417.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/Watzit/static/js/2.8ad82417.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d2560725be726bb6e992",
+    "url": "/Watzit/static/js/main.0cea30da.chunk.js"
+  },
+  {
+    "revision": "c764eee7daaf6e0d7ab3",
+    "url": "/Watzit/static/js/runtime-main.ae9f95f0.js"
+  }
+]);
